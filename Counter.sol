996dba616dd0534f9394d2b38330515ca8798a9b
adem ilhan <63885595+mellivoro@users.noreply.github.com>
2022-12-18 22:44:59 +0300

    Update Counter.sol

--- Counter.sol
+++ Counter.sol
@@ -3,13 +3,15 @@ pragma solidity ^0.8.13;
 
 contract Counter {
     uint public count;
-
+    // get current count
    function get() public view returns(uint){
        return count;
    }
+   //increase the current value
    function inc() public{
        count+=1;
    }
+   //decrease the current value
    function dec() public{
        count-=1;
    }
